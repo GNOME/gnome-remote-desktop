d51174689d184e79e07d54390330014fbe237bb1
Jonas Ådahl <jadahl@gmail.com>
2020-03-10 08:41:14 +0100

    session: Set 'disable-animations' property
    This will cause GNOME Shell to disable animations while a VNC session is
    active.

--- src/grd-session.c
+++ src/grd-session.c
@@ -407,6 +407,9 @@ on_remote_desktop_session_proxy_acquired (GObject      *object,
   g_variant_builder_add (&properties_builder, "{sv}",
                          "remote-desktop-session-id",
                          g_variant_new_string (remote_desktop_session_id));
+  g_variant_builder_add (&properties_builder, "{sv}",
+                         "disable-animations",
+                         g_variant_new_boolean (TRUE));
   properties_variant = g_variant_builder_end (&properties_builder);
 
   screen_cast_proxy = grd_context_get_screen_cast_proxy (priv->context);
