1a8e236134aaaefff79d971368a316e3aecacd63
Pascal Nowack <Pascal.Nowack@gmx.de>
2020-05-28 13:29:53 +0200

    pipewire: Add pipewire-utils
    When we implement other backends other than the already existing VNC
    backend, some functions and attributes in the pipewire backend of the
    new backend are either simply the same or can (and should) be shared
    with the other pipewire implementation due to unnecessary duplication.
    Pipewire-utils picks these functions and attributes up.

---
+++ src/grd-pipewire-utils.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2015 Red Hat Inc.
+ * Copyright (C) 2020 Pascal Nowack
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#include "config.h"
+
+#include "grd-pipewire-utils.h"
+
+#include <linux/dma-buf.h>
+#include <pipewire/pipewire.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+
+static gboolean is_pipewire_initialized = FALSE;
+
+void
+grd_maybe_initialize_pipewire (void)
+{
+  if (!is_pipewire_initialized)
+    {
+      pw_init (NULL, NULL);
+      is_pipewire_initialized = TRUE;
+    }
+}
+
+void
+grd_sync_dma_buf (int      fd,
+                  uint64_t start_or_end)
+{
+  struct dma_buf_sync sync = { 0 };
+
+  sync.flags = start_or_end | DMA_BUF_SYNC_READ;
+
+  while (TRUE)
+    {
+      int ret;
+
+      ret = ioctl (fd, DMA_BUF_IOCTL_SYNC, &sync);
+      if (ret == -1 && errno == EINTR)
+        {
+          continue;
+        }
+      else if (ret == -1)
+        {
+          g_warning ("Failed to synchronize DMA buffer: %s",
+                     g_strerror (errno));
+          break;
+        }
+      else
+        {
+          break;
+        }
+    }
+}
---
+++ src/grd-pipewire-utils.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2020 Pascal Nowack
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#ifndef GRD_PIPEWIRE_UTILS_H
+#define GRD_PIPEWIRE_UTILS_H
+
+#include <gio/gio.h>
+#include <stdint.h>
+
+#define CURSOR_META_SIZE(width, height) \
+ (sizeof(struct spa_meta_cursor) + \
+  sizeof(struct spa_meta_bitmap) + width * height * 4)
+
+typedef struct _GrdPipeWireSource
+{
+  GSource base;
+
+  struct pw_loop *pipewire_loop;
+} GrdPipeWireSource;
+
+void grd_maybe_initialize_pipewire (void);
+
+void grd_sync_dma_buf (int      fd,
+                       uint64_t start_or_end);
+
+#endif /* GRD_PIPEWIRE_UTILS_H */
