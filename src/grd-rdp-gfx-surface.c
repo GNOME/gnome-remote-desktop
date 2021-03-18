66df97a8f575739081e8d5c1b317017e80ac277b
Pascal Nowack <Pascal.Nowack@gmx.de>
2021-03-18 19:51:16 +0100

    rdp: Add skeleton class for GFX surfaces
    This class will later represent a GFX surface.
    
    Add the class now, so it can be already tracked in the corresponding
    RDP surface.

---
+++ src/grd-rdp-gfx-surface.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2021 Pascal Nowack
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#include "config.h"
+
+#include "grd-rdp-gfx-surface.h"
+
+struct _GrdRdpGfxSurface
+{
+  GObject parent;
+};
+
+G_DEFINE_TYPE (GrdRdpGfxSurface, grd_rdp_gfx_surface, G_TYPE_OBJECT);
+
+static void
+grd_rdp_gfx_surface_init (GrdRdpGfxSurface *gfx_surface)
+{
+}
+
+static void
+grd_rdp_gfx_surface_class_init (GrdRdpGfxSurfaceClass *klass)
+{
+}
