GNOME/gnome-remote-desktop

commit b17d0befe5e6d70466ac9d0110a6247499b92227

settings: Set error if username or password is NULL
When looking up the username or password for an RDP session,
gnome-remote-desktop will parse the credentials string, returned via
libsecret.
This credentials string may exist, but it can contain garbage values,
when e.g. the user changed them to such garbage values.
In such case, the parsed username or password may be NULL, leading into
gnome-remote-desktop to crash, as the error is not set.

Fix this behaviour by always setting the GError, when the username or
password is NULL.

src/grd-settings.c
@@ -158,6 +158,12 @@ grd_settings_get_rdp_username (GrdSettings  *settings,
 
   credentials = g_variant_parse (NULL, credentials_string, NULL, NULL, NULL);
   g_variant_lookup (credentials, "username", "s", &username);
+  if (!username)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_NOT_FOUND,
+                   "Username not set");
+      return NULL;
+    }
 
   g_variant_unref (credentials);
   g_free (credentials_string);
@@ -190,6 +196,12 @@ grd_settings_get_rdp_password (GrdSettings  *settings,
 
   credentials = g_variant_parse (NULL, credentials_string, NULL, NULL, NULL);
   g_variant_lookup (credentials, "password", "s", &password);
+  if (!password)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_NOT_FOUND,
+                   "Password not set");
+      return NULL;
+    }
 
   g_variant_unref (credentials);
   g_free (credentials_string);

commit 41d407a1888b84df71476b7e57ffebfce1a3679b

settings: Don't leak credentials, when username or password is NULL
While commit b17d0befe5e6d70466ac9d0110a6247499b92227 fixes a crash,
when the username or password is NULL, it accidentally leaks the string
with the credentials, which is in such case not NULL.

Use the autofree- and autoptr- helpers to get rid of any leaks here.

Fixes: b17d0befe5e6d70466ac9d0110a6247499b92227

src/grd-settings.c
@@ -138,8 +138,8 @@ grd_settings_get_rdp_username (GrdSettings  *settings,
                                GError      **error)
 {
   const char *test_password_override;
-  GVariant *credentials;
-  char *credentials_string;
+  g_autofree char *credentials_string = NULL;
+  g_autoptr (GVariant) credentials = NULL;
   char *username = NULL;
 
   test_password_override = g_getenv ("GNOME_REMOTE_DESKTOP_TEST_RDP_PASSWORD");
@@ -165,9 +165,6 @@ grd_settings_get_rdp_username (GrdSettings  *settings,
       return NULL;
     }
 
-  g_variant_unref (credentials);
-  g_free (credentials_string);
-
   return username;
 }
 
@@ -176,8 +173,8 @@ grd_settings_get_rdp_password (GrdSettings  *settings,
                                GError      **error)
 {
   const char *test_password_override;
-  GVariant *credentials;
-  char *credentials_string;
+  g_autofree char *credentials_string = NULL;
+  g_autoptr (GVariant) credentials = NULL;
   char *password = NULL;
 
   test_password_override = g_getenv ("GNOME_REMOTE_DESKTOP_TEST_RDP_PASSWORD");
@@ -202,9 +199,6 @@ grd_settings_get_rdp_password (GrdSettings  *settings,
                    "Password not set");
       return NULL;
     }
-
-  g_variant_unref (credentials);
-  g_free (credentials_string);
 
   return password;
 }