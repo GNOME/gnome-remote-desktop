eea0e3c0d5a3fb6232f899cf80e29d568e6c066d
Pascal Nowack <Pascal.Nowack@gmx.de>
2022-06-18 08:31:28 +0200

    session-rdp: Add handling to tear down display control channel
    This allows tearing down the display control channel, when e.g. the DVC
    creation failed.

--- src/grd-session-rdp.c
+++ src/grd-session-rdp.c
@@ -818,6 +818,9 @@ grd_session_rdp_tear_down_channel (GrdSessionRdp *session_rdp,
     case GRD_RDP_CHANNEL_AUDIO_PLAYBACK:
       g_clear_object (&rdp_peer_context->audio_playback);
       break;
+    case GRD_RDP_CHANNEL_DISPLAY_CONTROL:
+      g_clear_object (&rdp_peer_context->display_control);
+      break;
     case GRD_RDP_CHANNEL_TELEMETRY:
       g_clear_object (&rdp_peer_context->telemetry);
       break;
