8b0e2288de0c717d6d26e9d59440441058951ce2
Pascal Nowack <Pascal.Nowack@gmx.de>
2023-03-12 14:10:45 +0100

    session-rdp: Add error info for failed stream creations
    This new error info will be used by the layout manager.

--- src/grd-session-rdp.c
+++ src/grd-session-rdp.c
@@ -712,6 +712,9 @@ grd_session_rdp_notify_error (GrdSessionRdp      *session_rdp,
     case GRD_SESSION_RDP_ERROR_BAD_MONITOR_DATA:
       session_rdp->rdp_error_info = ERRINFO_BAD_MONITOR_DATA;
       break;
+    case GRD_SESSION_RDP_ERROR_CLOSE_STACK_ON_DRIVER_FAILURE:
+      session_rdp->rdp_error_info = ERRINFO_CLOSE_STACK_ON_DRIVER_FAILURE;
+      break;
     case GRD_SESSION_RDP_ERROR_GRAPHICS_SUBSYSTEM_FAILED:
       session_rdp->rdp_error_info = ERRINFO_GRAPHICS_SUBSYSTEM_FAILED;
       break;
