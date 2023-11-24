ee0dd5835447c7e9e6334061757989a24ee38e67
Pascal Nowack <Pascal.Nowack@gmx.de>
2023-11-24 14:46:23 +0100

    rdp: Add renderer class
    Currently, all rendering actions happen in the session-rdp class. This
    was fine for a long time, since gnome-remote-desktop only needed to
    handle one monitor.
    But even with just one monitor, the session-rdp class is pretty
    cluttered.
    
    To take care of this (and of future challenges), introduce a new
    renderer class. This class will manage all render actions, such as
    inhibition of rendering, or anything else, that is related to rendering,
    but not specific to certain surfaces.
    For now, add handling for the graphics thread, so it can be moved into
    this class in the next commit. And also add the surface disposal source
    into this class, which currently resides in the layout manager, where it
    should not be.
    In future commits, more handling is moved into the renderer, such as the
    rendering inhibition or the graphics reset handling.

---
+++ src/grd-rdp-renderer.c
@@ -0,0 +1,203 @@
+/*
+ * Copyright (C) 2023 Pascal Nowack
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#include "config.h"
+
+#include "grd-rdp-renderer.h"
+
+#include "grd-hwaccel-nvidia.h"
+#include "grd-rdp-surface.h"
+#include "grd-rdp-surface-renderer.h"
+
+struct _GrdRdpRenderer
+{
+  GObject parent;
+
+  gboolean in_shutdown;
+
+  GrdSessionRdp *session_rdp;
+  GrdHwAccelNvidia *hwaccel_nvidia;
+
+  GThread *graphics_thread;
+  GMainContext *graphics_context;
+
+  GSource *surface_disposal_source;
+  GAsyncQueue *disposal_queue;
+};
+
+G_DEFINE_TYPE (GrdRdpRenderer, grd_rdp_renderer, G_TYPE_OBJECT)
+
+GMainContext *
+grd_rdp_renderer_get_graphics_context (GrdRdpRenderer *renderer)
+{
+  return renderer->graphics_context;
+}
+
+void
+grd_rdp_renderer_invoke_shutdown (GrdRdpRenderer *renderer)
+{
+  g_assert (renderer->graphics_context);
+  g_assert (renderer->graphics_thread);
+
+  renderer->in_shutdown = TRUE;
+
+  g_main_context_wakeup (renderer->graphics_context);
+  g_clear_pointer (&renderer->graphics_thread, g_thread_join);
+}
+
+GrdRdpSurface *
+grd_rdp_renderer_try_acquire_surface (GrdRdpRenderer *renderer,
+                                      uint32_t        refresh_rate)
+{
+  GrdRdpSurface *rdp_surface;
+  GrdRdpSurfaceRenderer *surface_renderer;
+
+  rdp_surface = grd_rdp_surface_new (renderer->hwaccel_nvidia);
+  if (!rdp_surface)
+    return NULL;
+
+  surface_renderer = grd_rdp_surface_renderer_new (rdp_surface,
+                                                   renderer->graphics_context,
+                                                   renderer->session_rdp,
+                                                   refresh_rate);
+  grd_rdp_surface_attach_surface_renderer (rdp_surface, surface_renderer);
+
+  return rdp_surface;
+}
+
+void
+grd_rdp_renderer_release_surface (GrdRdpRenderer *renderer,
+                                  GrdRdpSurface  *rdp_surface)
+{
+  g_assert (rdp_surface);
+
+  g_async_queue_push (renderer->disposal_queue, rdp_surface);
+  g_source_set_ready_time (renderer->surface_disposal_source, 0);
+}
+
+static gpointer
+graphics_thread_func (gpointer data)
+{
+  GrdRdpRenderer *renderer = data;
+
+  if (renderer->hwaccel_nvidia)
+    grd_hwaccel_nvidia_push_cuda_context (renderer->hwaccel_nvidia);
+
+  while (!renderer->in_shutdown)
+    g_main_context_iteration (renderer->graphics_context, TRUE);
+
+  if (renderer->hwaccel_nvidia)
+    grd_hwaccel_nvidia_pop_cuda_context (renderer->hwaccel_nvidia);
+
+  return NULL;
+}
+
+GrdRdpRenderer *
+grd_rdp_renderer_new (GrdSessionRdp    *session_rdp,
+                      GrdHwAccelNvidia *hwaccel_nvidia)
+{
+  GrdRdpRenderer *renderer;
+
+  renderer = g_object_new (GRD_TYPE_RDP_RENDERER, NULL);
+  renderer->session_rdp = session_rdp;
+  renderer->hwaccel_nvidia = hwaccel_nvidia;
+
+  renderer->graphics_thread = g_thread_new ("RDP graphics thread",
+                                            graphics_thread_func,
+                                            renderer);
+
+  return renderer;
+}
+
+static gboolean
+dispose_surfaces (gpointer user_data)
+{
+  GrdRdpRenderer *renderer = user_data;
+  GrdRdpSurface *rdp_surface;
+
+  while ((rdp_surface = g_async_queue_try_pop (renderer->disposal_queue)))
+    {
+      g_clear_object (&rdp_surface->gfx_surface);
+      grd_rdp_surface_free (rdp_surface);
+    }
+
+  return G_SOURCE_CONTINUE;
+}
+
+static void
+grd_rdp_renderer_dispose (GObject *object)
+{
+  GrdRdpRenderer *renderer = GRD_RDP_RENDERER (object);
+
+  if (renderer->graphics_thread)
+    grd_rdp_renderer_invoke_shutdown (renderer);
+
+  if (renderer->surface_disposal_source)
+    {
+      g_source_destroy (renderer->surface_disposal_source);
+      g_clear_pointer (&renderer->surface_disposal_source, g_source_unref);
+    }
+
+  g_clear_pointer (&renderer->graphics_context, g_main_context_unref);
+
+  dispose_surfaces (renderer);
+  g_clear_pointer (&renderer->disposal_queue, g_async_queue_unref);
+
+  G_OBJECT_CLASS (grd_rdp_renderer_parent_class)->dispose (object);
+}
+
+static gboolean
+source_dispatch (GSource     *source,
+                 GSourceFunc  callback,
+                 gpointer     user_data)
+{
+  g_source_set_ready_time (source, -1);
+
+  return callback (user_data);
+}
+
+static GSourceFuncs source_funcs =
+{
+  .dispatch = source_dispatch,
+};
+
+static void
+grd_rdp_renderer_init (GrdRdpRenderer *renderer)
+{
+  GSource *surface_disposal_source;
+
+  renderer->disposal_queue = g_async_queue_new ();
+
+  renderer->graphics_context = g_main_context_new ();
+
+  surface_disposal_source = g_source_new (&source_funcs, sizeof (GSource));
+  g_source_set_callback (surface_disposal_source, dispose_surfaces,
+                         renderer, NULL);
+  g_source_set_ready_time (surface_disposal_source, -1);
+  g_source_attach (surface_disposal_source, renderer->graphics_context);
+  renderer->surface_disposal_source = surface_disposal_source;
+}
+
+static void
+grd_rdp_renderer_class_init (GrdRdpRendererClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->dispose = grd_rdp_renderer_dispose;
+}
---
+++ src/grd-rdp-renderer.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2023 Pascal Nowack
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#ifndef GRD_RDP_RENDERER_H
+#define GRD_RDP_RENDERER_H
+
+#include <glib-object.h>
+#include <stdint.h>
+
+#include "grd-types.h"
+
+#define GRD_TYPE_RDP_RENDERER (grd_rdp_renderer_get_type ())
+G_DECLARE_FINAL_TYPE (GrdRdpRenderer, grd_rdp_renderer,
+                      GRD, RDP_RENDERER, GObject)
+
+GrdRdpRenderer *grd_rdp_renderer_new (GrdSessionRdp    *session_rdp,
+                                      GrdHwAccelNvidia *hwaccel_nvidia);
+
+GMainContext *grd_rdp_renderer_get_graphics_context (GrdRdpRenderer *renderer);
+
+void grd_rdp_renderer_invoke_shutdown (GrdRdpRenderer *renderer);
+
+GrdRdpSurface *grd_rdp_renderer_try_acquire_surface (GrdRdpRenderer *renderer,
+                                                     uint32_t        refresh_rate);
+
+void grd_rdp_renderer_release_surface (GrdRdpRenderer *renderer,
+                                       GrdRdpSurface  *rdp_surface);
+
+#endif /* GRD_RDP_RENDERER_H */
