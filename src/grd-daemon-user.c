058164b23787d5c0df27bc87f1e8908ecc81bbc5
Joan Torres <joan.torres@suse.com>
2024-01-10 11:55:43 +0100

    daemon-user: Restart the RDP-server when there's a port conflict
    The daemon-user stores a dbus proxy of the daemon-system Rdp.Server
    iface.
    That interface informs listeners, when the RDP-server is about to bind
    to a port, that is passed to the Binding signal.
    The daemon-user connects to that signal and when it's emitted, checks
    if the binding port is the same one, the daemon-user is currently bound
    to. Then when the ports are the same, the daemon-user restarts
    the RDP-server with a delay.

--- src/grd-daemon-user.c
+++ src/grd-daemon-user.c
@@ -29,9 +29,53 @@
 struct _GrdDaemonUser
 {
   GrdDaemon parent;
+
+  GrdDBusRemoteDesktopRdpServer *system_rdp_server;
 };
 
 G_DEFINE_TYPE (GrdDaemonUser, grd_daemon_user, GRD_TYPE_DAEMON)
+
+static void
+on_system_rdp_server_binding (GrdDBusRemoteDesktopRdpServer *system_rdp_server,
+                              int                            system_rdp_port,
+                              GrdDaemonUser                 *daemon_user)
+{
+  GrdContext *context = grd_daemon_get_context (GRD_DAEMON (daemon_user));
+  GrdDBusRemoteDesktopRdpServer *user_rdp_server =
+    grd_context_get_rdp_server_interface (context);
+  int user_rdp_port =
+    grd_dbus_remote_desktop_rdp_server_get_port (user_rdp_server);
+
+  if (system_rdp_port != user_rdp_port)
+    return;
+
+  g_debug ("[DaemonUser] Restarting RDP server due to port conflict with the "
+           "system daemon");
+
+  grd_daemon_restart_rdp_server_with_delay (GRD_DAEMON (daemon_user));
+}
+
+static void
+on_remote_desktop_rdp_server_proxy_acquired (GObject      *object,
+                                             GAsyncResult *result,
+                                             gpointer      user_data)
+{
+  GrdDaemonUser *daemon_user = user_data;
+  g_autoptr (GError) error = NULL;
+
+  daemon_user->system_rdp_server =
+    grd_dbus_remote_desktop_rdp_server_proxy_new_for_bus_finish (result, &error);
+  if (!daemon_user->system_rdp_server)
+    {
+      g_warning ("[DaemonUser] Failed to create system rdp server proxy: "
+                 "%s", error->message);
+      return;
+    }
+
+  g_signal_connect (daemon_user->system_rdp_server, "binding",
+                    G_CALLBACK (on_system_rdp_server_binding),
+                    daemon_user);
+}
 
 GrdDaemonUser *
 grd_daemon_user_new (GrdRuntimeMode   runtime_mode,
@@ -75,14 +119,35 @@ grd_daemon_user_init (GrdDaemonUser *daemon_user)
 static void
 grd_daemon_user_startup (GApplication *app)
 {
-  GrdDaemon *daemon = GRD_DAEMON (app);
+  GrdDaemonUser *daemon_user = GRD_DAEMON_USER (app);
+  GCancellable *cancellable =
+    grd_daemon_get_cancellable (GRD_DAEMON (daemon_user));
 
-  grd_daemon_acquire_mutter_dbus_proxies (daemon);
+  grd_daemon_acquire_mutter_dbus_proxies (GRD_DAEMON (daemon_user));
 
-  g_signal_connect (daemon, "mutter-proxy-acquired",
+  g_signal_connect (daemon_user, "mutter-proxy-acquired",
                     G_CALLBACK (grd_daemon_maybe_enable_services), NULL);
 
+  grd_dbus_remote_desktop_rdp_server_proxy_new_for_bus (
+    G_BUS_TYPE_SYSTEM,
+    G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START,
+    REMOTE_DESKTOP_BUS_NAME,
+    GRD_RDP_SERVER_OBJECT_PATH,
+    cancellable,
+    on_remote_desktop_rdp_server_proxy_acquired,
+    daemon_user);
+
   G_APPLICATION_CLASS (grd_daemon_user_parent_class)->startup (app);
+}
+
+static void
+grd_daemon_user_shutdown (GApplication *app)
+{
+  GrdDaemonUser *daemon_user = GRD_DAEMON_USER (app);
+
+  g_clear_object (&daemon_user->system_rdp_server);
+
+  G_APPLICATION_CLASS (grd_daemon_user_parent_class)->shutdown (app);
 }
 
 static gboolean
@@ -104,6 +169,7 @@ grd_daemon_user_class_init (GrdDaemonUserClass *klass)
   GrdDaemonClass *daemon_class = GRD_DAEMON_CLASS (klass);
 
   g_application_class->startup = grd_daemon_user_startup;
+  g_application_class->shutdown = grd_daemon_user_shutdown;
 
   daemon_class->is_daemon_ready = grd_daemon_user_is_ready;
 }
