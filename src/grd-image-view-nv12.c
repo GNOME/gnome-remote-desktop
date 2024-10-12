4b10e4f1c6b685bae047c8c7b994a9cdd4c78498
Pascal Nowack <Pascal.Nowack@gmx.de>
2024-10-12 14:03:28 +0200

    Add image view for NV12 images
    For AVC420, an NV12 surface is needed. An NV12 surface contains two
    layers:
    One Y-layer for the luma data, and one UV-layer for the subsampled
    chroma data. In RDPGFX, this is called the main view.
    For AVC444, two NV12 surfaces will be needed: One for the main view and
    one for the auxiliary view.
    The reason for this is, that in RDPGFX, one AVC420 stream is used for
    AVC444. The auxiliary view here contains the missing chroma data from
    the main view. After decoding both views (subframes), the client
    reassembles the actual frame. Microsoft added this way to RDP, as at the
    time of creation, most hardware encoders did not support native AVC444.
    AMD GPUs today still don't support native AVC444, but just AVC420.
    
    The image view here in gnome-remote-desktop will be used for each of
    those views.
    Theoretically, the NV12 image view could just contain one VkImage
    containing both the Y- and UV-layer. However, the Intel driver does not
    support multiple image layers in Vulkan, although Vulkan supports that.
    So use for each layers an individual VkImage.

---
+++ src/grd-image-view-nv12.c
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2024 Pascal Nowack
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#include "config.h"
+
+#include "grd-image-view-nv12.h"
+
+struct _GrdImageViewNV12
+{
+  GrdImageView parent;
+
+  GrdVkImage *vk_y_layer;
+  GrdVkImage *vk_uv_layer;
+};
+
+G_DEFINE_TYPE (GrdImageViewNV12, grd_image_view_nv12,
+               GRD_TYPE_IMAGE_VIEW)
+
+GrdVkImage *
+grd_image_view_nv12_get_y_layer (GrdImageViewNV12 *image_view_nv12)
+{
+  return image_view_nv12->vk_y_layer;
+}
+
+GrdVkImage *
+grd_image_view_nv12_get_uv_layer (GrdImageViewNV12 *image_view_nv12)
+{
+  return image_view_nv12->vk_uv_layer;
+}
+
+GrdImageViewNV12 *
+grd_image_view_nv12_new (GrdVkImage *vk_y_layer,
+                         GrdVkImage *vk_uv_layer)
+{
+  GrdImageViewNV12 *image_view_nv12;
+
+  image_view_nv12 = g_object_new (GRD_TYPE_IMAGE_VIEW_NV12, NULL);
+  image_view_nv12->vk_y_layer = vk_y_layer;
+  image_view_nv12->vk_uv_layer = vk_uv_layer;
+
+  return image_view_nv12;
+}
+
+static void
+grd_image_view_nv12_dispose (GObject *object)
+{
+  GrdImageViewNV12 *image_view_nv12 = GRD_IMAGE_VIEW_NV12 (object);
+
+  g_clear_object (&image_view_nv12->vk_uv_layer);
+  g_clear_object (&image_view_nv12->vk_y_layer);
+
+  G_OBJECT_CLASS (grd_image_view_nv12_parent_class)->dispose (object);
+}
+
+static void
+grd_image_view_nv12_init (GrdImageViewNV12 *image_view_nv12)
+{
+}
+
+static void
+grd_image_view_nv12_class_init (GrdImageViewNV12Class *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->dispose = grd_image_view_nv12_dispose;
+}
