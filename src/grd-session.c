GNOME/gnome-remote-desktop

commit 6153f1920db5ada529455d4c38681f595876347f

session: Only emit 'ready' once libei is connected
This avoids async flushes hitting an assert in libei if done before
being connected.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-remote-desktop/-/merge_requests/345>

src/grd-session.c
@@ -240,6 +240,8 @@ grd_session_flush_input_async (GrdSession          *session,
   GrdSessionPrivate *priv = grd_session_get_instance_private (session);
   GrdEiPing *ping;
 
+  g_assert (priv->is_ready);
+
   ping = g_new0 (GrdEiPing, 1);
   ping->task = g_task_new (session,
                            cancellable,
@@ -1419,6 +1421,12 @@ grd_ei_source_dispatch (gpointer user_data)
       switch (ei_event_type)
         {
         case EI_EVENT_CONNECT:
+          if (!priv->is_ready)
+            {
+              priv->is_ready = TRUE;
+              g_signal_emit (session, signals[READY], 0);
+            }
+          break;
         case EI_EVENT_DISCONNECT:
           break;
         case EI_EVENT_SEAT_ADDED:
@@ -1688,9 +1696,6 @@ on_eis_connected (GObject      *object,
                                                    priv->cancellable,
                                                    on_screen_cast_session_created,
                                                    session);
-
-  priv->is_ready = TRUE;
-  g_signal_emit (session, signals[READY], 0);
 }
 
 static void

commit c787088edb045fad7e78b4aacbf616ec92a2c5f6

session: Fix race condition between ready and started signal
The event handling of the ready and started signal, especially in the
RDP backend, assumes that the ready signal is always called before the
started signal. With commit 6153f1920db5ada529455d4c38681f595876347f,
this assumption is now not necessarily true anymore. And so it is now
possible that the started signal is called before the ready signal.
In order to fix this issue, delay the started the signal until after the
ready signal in case the started signal emitter is handled before the
ready signal emitter.

Fixes: 6153f1920db5ada529455d4c38681f595876347f
Part-of: <https://gitlab.gnome.org/GNOME/gnome-remote-desktop/-/merge_requests/356>

src/grd-session.c
@@ -1035,6 +1035,18 @@ grd_session_selection_read (GrdSession  *session,
 }
 
 static void
+maybe_emit_started_signal (GrdSession *session)
+{
+  GrdSessionPrivate *priv = grd_session_get_instance_private (session);
+
+  if (!priv->is_ready ||
+      !priv->started)
+    return;
+
+  g_signal_emit (session, signals[STARTED], 0);
+}
+
+static void
 on_session_start_finished (GObject      *object,
                            GAsyncResult *result,
                            gpointer      user_data)
@@ -1061,7 +1073,7 @@ on_session_start_finished (GObject      *object,
   priv = grd_session_get_instance_private (session);
 
   priv->started = TRUE;
-  g_signal_emit (session, signals[STARTED], 0);
+  maybe_emit_started_signal (session);
 }
 
 static void
@@ -1425,6 +1437,7 @@ grd_ei_source_dispatch (gpointer user_data)
             {
               priv->is_ready = TRUE;
               g_signal_emit (session, signals[READY], 0);
+              maybe_emit_started_signal (session);
             }
           break;
         case EI_EVENT_DISCONNECT: